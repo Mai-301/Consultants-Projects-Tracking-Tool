797edce73c79017b2b5ab27edd8adcfcec07d9aa
Mai Sadek <mmsadek97@gmail.com>
2017-02-25 01:57:07 +0200

    updating some components

--- time-tracking-tool/src/app/app.module.ts
+++ time-tracking-tool/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
@@ -10,6 +9,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { ProjectComponent } from './project/project.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { SubActivitiesComponent } from './sub-activities/sub-activities.component';
+
 
 @NgModule({
   declarations: [
